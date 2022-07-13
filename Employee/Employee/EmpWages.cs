970a511e4a654a66e56f76d3d6b0198fdab1e9a4
poojaamar <pamar6401@gmail.com>
2022-07-13 11:29:27 +0100

    added uc-1

---
+++ Employee/Employee/EmpWages.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    internal class EmpWages
+    {
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+        public void Attendence()
+        {
+
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+
+
+    }
+}
---
+++ Employee/Employee/Program.cs
@@ -0,0 +1,4 @@
+﻿using Employee;
+
+EmpWages abc=new EmpWages();
+abc.Attendence();
