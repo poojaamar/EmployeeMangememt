c7e856179e9e479132b64addeb5f01b109f0720d
poojaamar <pamar6401@gmail.com>
2022-07-13 17:05:39 +0100

    uc-1 present absent

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -1,4 +1,14 @@
-﻿using Employee;
+﻿int IS_FULL_TIME = 1;
+Random random = new Random();
+int empCheck = random.Next(0, 2);
+if (empCheck == IS_FULL_TIME)
+{
+    Console.WriteLine("Employee is Present");
 
-EmpWages abc=new EmpWages();
-abc.Attendence();
+}
+else
+{
+    Console.WriteLine("Employee is Absent");
+}
+
+
