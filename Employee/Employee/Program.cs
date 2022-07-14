e969aaa3c3f73938be94db1e37de54e4354d062a
poojaamar <pamar6401@gmail.com>
2022-07-14 15:42:30 +0100

    uc-7 computeEmpWage

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -3,28 +3,33 @@ const int IS_FULL_TIME = 2;
 const int EMP_RATE_PER_HOUR = 20;
 const int NUM_OF_WORKING_DAYS = 2;
 const int MAX_HRS_IN_MONTH = 10;
-int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+
+ static int computeEmpWage()
 {
-    totalWorkingDays++;
-    Random random = new Random();
-    int empCheck = random.Next(0, 3);
-    switch (empCheck)
+    int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+    while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
     {
-        case IS_PART_TIME:
-            empHrs = 4;
-            break;
-        case IS_FULL_TIME:
-            empHrs = 8;
-            break;
-        default:
-            empHrs = 0;
-            break;
+        totalWorkingDays++;
+        Random random = new Random();
+        int empCheck = random.Next(0, 3);
+        switch (empCheck)
+        {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+                break;
+        }
+        totalEmpHrs += empHrs;
+        Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
     }
-    totalEmpHrs += empHrs; 
-    Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs); 
+    int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+    Console.WriteLine("total Emp Wage : " + totalEmpWage);
+    return totalEmpWage;
+ }
 
-}
-int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-Console.WriteLine("total Emp Wage : " + totalEmpWage);
-
+    computeEmpWage();
