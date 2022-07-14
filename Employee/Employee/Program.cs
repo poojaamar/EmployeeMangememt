5086b3c838d7ed7dbb2df07d3735f979ad5a5c29
poojaamar <pamar6401@gmail.com>
2022-07-14 03:32:49 +0100

    uc-6 total working days 100 or max days

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -2,9 +2,11 @@
 const int IS_FULL_TIME = 2; 
 const int EMP_RATE_PER_HOUR = 20;
 const int NUM_OF_WORKING_DAYS = 2;
-int empHrs = 0, empWage = 0, totalEmpWage = 0;
-for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+const int MAX_HRS_IN_MONTH = 10;
+int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 {
+    totalWorkingDays++;
     Random random = new Random();
     int empCheck = random.Next(0, 3);
     switch (empCheck)
@@ -19,9 +21,10 @@ for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             empHrs = 0;
             break;
     }
-    empWage = empHrs * EMP_RATE_PER_HOUR;
-    totalEmpWage += empWage;
-    Console.WriteLine("Emp Wage: " + empWage);
+    totalEmpHrs += empHrs; 
+    Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs); 
+
 }
+int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 Console.WriteLine("total Emp Wage : " + totalEmpWage);
 
