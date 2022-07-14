d5922b0094faf79d78156a98aa2b76c8ab5a545b
poojaamar <pamar6401@gmail.com>
2022-07-14 18:42:02 +0100

    uc-9 EmpWageBuilderObject

---
+++ Employee/Employee/EmpWageBuilderObject.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    internal class EmpWageBuilderObject
+    {
+    }
+}
+public class EmpWageBuilderObject
+{
+
+    const int IS_PART_TIME = 1;
+    const int IS_FULL_TIME = 2;
+
+    private string company;
+    private int empRatePerHrs;
+    private int numOfWorkingDays;
+    private int maxHrsPerMonth;
+    private int totalEmpWage;
+
+    public EmpWageBuilderObject(string company, int empRatePerHrs, int numOfWorkingDays, int maxHrsPerMonth)
+    {
+        this.company = company;
+        this.empRatePerHrs = empRatePerHrs;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHrsPerMonth = maxHrsPerMonth;
+    }
+    public void computeEmpWage()
+    {
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+
+        while (totalEmpHrs <= this.maxHrsPerMonth && totalWorkingDays < this.numOfWorkingDays)
+        {
+            totalWorkingDays++;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            totalEmpHrs += empHrs;
+            Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
+        }
+        totalEmpWage = totalEmpHrs * this.empRatePerHrs;
+        Console.WriteLine("total Emp Wage for company: " + company + " is: " + totalEmpWage);
+    }
+    public string toString()
+    {
+        return "total Emp Wage for company; " + this. company + " is; " + this.totalEmpWage;
+        
+    }
+
+
+}
+
--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -1,33 +1,7 @@
-﻿const int IS_PART_TIME = 1;
-const int IS_FULL_TIME = 2; 
-
- static int computeEmpWage(string company, int empRatePerHrs,int numOFWorkingDays,int maxHrsPerMonth)
- {
-    int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-    while (totalEmpHrs <= maxHrsPerMonth && totalWorkingDays < numOFWorkingDays)
-    {
-        totalWorkingDays++;
-        Random random = new Random();
-        int empCheck = random.Next(0, 3);
-        switch (empCheck)
-        {
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            default:
-                empHrs = 0;
-                break;
-        }
-        totalEmpHrs += empHrs;
-        Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
-    }
-    int totalEmpWage = totalEmpHrs * empRatePerHrs;
-    Console.WriteLine("total Emp Wage for company: " + company + " is: " + totalEmpWage);
-    return totalEmpWage;
- }
-
-    computeEmpWage("Dmart",20,2,30);
-    computeEmpWage("Reliance", 10, 4, 20);
+﻿using Employee;
+EmpWageBuilderObject dMart = new EmpWageBuilderObject("Dmart", 20, 2, 10);
+EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+dMart.computeEmpWage();
+Console.WriteLine(dMart.toString());
+reliance.computeEmpWage();
+Console.WriteLine(reliance.toString());
