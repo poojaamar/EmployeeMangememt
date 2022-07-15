0465e38d39bf07ccbd0ec005ce1ef78639e6a356
poojaamar <pamar6401@gmail.com>
2022-07-15 16:41:28 +0100

    UC-10 ability multipal company

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Employee;
+
 EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
 empWageBuilder.addCompanyWage("Dmart", 20, 2, 10);
 empWageBuilder.addCompanyWage("Reliance", 10, 4, 20);
